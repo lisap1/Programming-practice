2a9b88abe8e627dabb742400ffef63a035743612
lisap1 <124928251+lisap1@users.noreply.github.com>
2023-02-16 13:58:38 +1300

    Comments Getting Started

---
+++ 00_MMF_base.py
@@ -0,0 +1,28 @@
+# import statements
+
+# functions go here
+
+
+# ******** Main Routine ********
+
+# set up dictionaries / lists needed to hold data
+
+# ask user if they have used the program before and show instructions if necessary
+
+# Loop to get tickets details
+
+    # Get name (can't be blank)
+
+    # Get age ( between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # ask for payment method ( and apply surcharge if necessary)
+
+# Calculate total sales and profit
+
+# Output data to text files

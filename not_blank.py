8ecad29e453b30e8844ab87b6dea63cd098972cc
lisap1 <124928251+lisap1@users.noreply.github.com>
2023-02-20 14:11:16 +1300

    name not blank
    Function to check their name is not blank

---
+++ not_blank.py
@@ -0,0 +1,19 @@
+# Functions go here
+
+# function for asking name
+
+def not_blank(question):
+    valid = False
+
+    # while valid= False, continue asking
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry - This can't be blank, please enter your name")
+
+        # Main Routine goes here
+        name = not_blank("Name:")

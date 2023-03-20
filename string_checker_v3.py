12e3ab4c81ef16eff46f81194448c48dd9f10eff
lisap1 <124928251+lisap1@users.noreply.github.com>
2023-03-21 10:43:32 +1300

    string checker v3 now works + comments

--- string_checker_v3.py
+++ string_checker_v3.py
@@ -1,3 +1,4 @@
+# function goes here
 def string_check(choice, options):
 
     for var_list in options:
@@ -15,6 +16,10 @@ def string_check(choice, options):
         return "invalid choice"
 
 
+# valid snacks holds list of all snacks
+# each item in valid snacks is a list with
+# valid options for each snack <full name, letter code (a - e)
+# , and possible abbreviations etc.>
 valid_snacks = [
     ["popcorn", "p", "corn", "a"],
     ["M&M's", "m&m's", "mms", "m", "m and m", "b"],
@@ -22,35 +27,47 @@ valid_snacks = [
     ["water", "w", "d"]
 ]
 
+# valid options for yes / no questions
 yes_no = [
     ["yes", "y"],
     ["no", "n"]
 ]
 
+# holds snack order for a single user.
 snack_order = []
 
+# ask user if they want a snack
 check_snack = "invalid choice"
 while check_snack == "invalid choice":
     want_snack = input("do you want to order snacks: ").lower()
     check_snack = string_check(want_snack, yes_no)
+    if check_snack == "invalid choice":
+        print("invalid - Please enter yes or no")
 
-if check_snack == "yes":
+# if they say yes, ask what snacks they want (and add to our snack order)
+if check_snack == "Yes":
     desired_snack = ""
     while desired_snack != "xxx":
+        # ask user for desired snack and put it in lowercase
         desired_snack = input("snack: ").lower()
 
         if desired_snack == "xxx":
             break
 
+        # check if snack is valid
         snack_choice = string_check(desired_snack, valid_snacks)
         print("snack choice: ", snack_choice)
 
+        # add to snack list...
+
+        # check that snack is ot the exit code before adding
         if snack_choice != "xxx" and snack_choice != "invalid choice":
             snack_order.append(snack_choice)
 
+# show snack orders
 print()
 if len(snack_order) == 0:
-    print("snacks Ordered: None")
+    print("Snacks Ordered: None")
 else:
     print("Snacks Ordered: ")
     for item in snack_order:
